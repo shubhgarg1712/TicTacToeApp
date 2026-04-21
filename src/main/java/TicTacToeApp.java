16c0deebcd76a925f995ab023da92fcc6236a5e1
shubh garg <shubhgarg1712@gmail.com>
2026-04-21 10:23:57 +0530

    Coded UC2: Implement Random Toss and assign symbols

--- src/main/java/TicTacToeApp.java
+++ src/main/java/TicTacToeApp.java
@@ -1,8 +1,14 @@
+import java.util.Random;
+
 public class TicTacToeApp {
     private static char[][] board = new char[3][3];
+    private static char currentPlayerSymbol;
+    private static char player1Symbol;
+    private static char player2Symbol;
 
     public static void main(String[] args) {
         initializeBoard();
+        performToss();
         printBoard();
     }
 
@@ -20,6 +26,23 @@ public class TicTacToeApp {
                 System.out.print(board[i][j] + " ");
             }
             System.out.println();
+        }
+    }
+
+    private static void performToss() {
+        Random rand = new Random();
+        int toss = rand.nextInt(2); // Generates 0 or 1
+        
+        if (toss == 0) {
+            System.out.println("Player 1 wins the toss and starts first!");
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+            currentPlayerSymbol = 'X';
+        } else {
+            System.out.println("Player 2 wins the toss and starts first!");
+            player2Symbol = 'X';
+            player1Symbol = 'O';
+            currentPlayerSymbol = 'X';
         }
     }
 }
